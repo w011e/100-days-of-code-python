633b276220e690d9ca9ecb1f470de35ec5bcd0f6
w011e <124792399+w011e@users.noreply.github.com>
2023-04-02 18:47:51 +0200

    Create day-1-printing.py

---
+++ day-1-printing.py
@@ -0,0 +1,21 @@
+# getting started 
+# print("Hi\nThere\nYo yo yo") 
+#add \n to jump to new line
+
+# string concatenation = taking separate strings and merge them into one 
+# print("Hello " + "you") 
+
+#debiggung erxercise
+
+#fixed the code below 👇
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+#Input function 
+# input() will get user input in console 
+# prin() will print the word "Hello" and user input 
+# input("A prompt for the user")
+# print("Hello " + input("What's your name?"))
+
