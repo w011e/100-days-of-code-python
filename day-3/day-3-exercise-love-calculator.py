510911423d5ab2ada1efc444960e2d94de7b6094
w011e <paulbiesold@posteo.de>
2023-04-06 19:46:12 +0200

    update

---
+++ day-3/day-3-exercise-love-calculator.py
@@ -0,0 +1,27 @@
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+
+combined_name = name1 + name2
+lower_combined_name = combined_name.lower()
+
+t = lower_combined_name.count("t")
+r = lower_combined_name.count("r")
+u = lower_combined_name.count("u")
+e = lower_combined_name.count("e")
+true = t + r + u + e
+
+l = lower_combined_name.count("l")
+o = lower_combined_name.count("o")
+v = lower_combined_name.count("v")
+e = lower_combined_name.count("e")
+love = l + o + v + e
+
+score = int(str(true) + str(love))
+
+if score < 10 or score > 90:
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif score >= 40 and score <= 50:
+    print(f"Your score is {score}, you are alright together.")
+else:
+    print(f"Your score is {score}.")
