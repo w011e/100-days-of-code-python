79dc21d48eca3830f1d227f41cc1b1d4ea7b21c7
w011e <paulbiesold@posteo.de>
2023-04-09 14:41:47 +0200

    update

--- day-4/day-4-exercise-banker-roulette.py
+++ day-4/day-4-exercise-banker-roulette.py
@@ -1,9 +1,13 @@
 import random 
+
 names_string = input("Give me everybody's names, separated by a comma. ")
 names = names_string.split(", ")
 
-number_of_people = len(names)
+# number_of_people = len(names)
 
-random_choice = random.randint(0, number_of_people-1)
-person_to_pay = names[random_choice]
+# random_choice = random.randint(0, number_of_people-1)
+# person_to_pay = names[random_choice]
+
+#option also to use random.choice()
+person_to_pay = random.choice(names)
 print(f"{person_to_pay} is going to buy the meal today!")
