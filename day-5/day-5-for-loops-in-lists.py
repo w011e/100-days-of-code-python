a57fb4ad9821b9b43ca82fda0ab79f4b1aee818d
w011e <paulbiesold@posteo.de>
2023-04-11 08:46:14 +0200

    update

---
+++ day-5/day-5-for-loops-in-lists.py
@@ -0,0 +1,6 @@
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Pie")
+#identation matters! here print(fruit) is not possible, since variable is only assigned within for loop 
+print(fruits)
