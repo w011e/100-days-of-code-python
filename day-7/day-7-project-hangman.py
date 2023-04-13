4702a2d390b6baf752b432616bf7b090839b762e
w011e <paulbiesold@posteo.de>
2023-04-14 00:17:46 +0200

    update

--- day-7/day-7-project-hangman.py
+++ day-7/day-7-project-hangman.py
@@ -1,19 +1,89 @@
-#Step 1 
-
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+end_of_game = False
 word_list = ["aardvark", "baboon", "camel"]
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+lives = 6
 
+print(chosen_word)
 
-#TODO-2 - Ask the user to guess a letter
-# assign their answer to a variable called guess. 
-# Make guess lowercase.
+display = []
+for letter in chosen_word:
+    display.append("_")
 
-guess = input("Please guess a letter: ")
-print(guess.lower)
+while not end_of_game:
+    guess = input("Please guess a letter: ").lower()
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    print(display) 
+
+    if lives == 0:
+        end_of_game = True
+        print("You lose.")
+    elif "_" not in display: 
+        end_of_game = True
+        print("You win")
+    elif guess not in chosen_word:
+        lives -= 1
+        print(stages[lives])
