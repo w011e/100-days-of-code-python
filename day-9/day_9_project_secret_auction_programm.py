49f72a4761c69f963ac4a7e8b73a1a39798e073b
w011e <paulbiesold@posteo.de>
2023-04-17 17:48:16 +0200

    update

--- day-9/day_9_project_secret_auction_programm.py
+++ day-9/day_9_project_secret_auction_programm.py
@@ -9,6 +9,7 @@ bidding_finished = False
 def find_highest_bidder(bidding_record):
     highest_bid = 0
     winner = ""
+    #bidding_record = {"bidder": "bid_amount"}
     for bidder in bidding_record:
         bid_amount = bidding_record[bidder]
         if bid_amount > highest_bid:
